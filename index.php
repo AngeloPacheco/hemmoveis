4ae5d9b580725c5a885bc4d95d5279749475aa24
AngeloPacheco <angelopsster@gmail.com>
2017-04-27 13:54:59 -0300

    retirada de jquery desnecessário

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		    <link href="css/estilo.css" rel="stylesheet">
 		    <link rel="shortcut icon" href="img/ico.fw.png" type="image/x-icon" />
 		
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+           
 
             <script>
 			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
